6b16f6abd6860a917d27f8a9d6e0c1f07b224127
Saikumar-arch <iamsaikumar630@gmai.com>
2025-10-19 13:47:17 +0530

    Developed Sign in and sign up buttons

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,18 +2,39 @@
 import React, { useState } from 'react'
 
 const Login = () => {
-   
+  
+  const [username, SetUsername] = useState("")
   const [emailId, SetEmailid] = useState("")
   const [password, SetPassword] = useState("")
-
+  const [issignin, SetSignin] = useState(true)
+  
+ 
+  const toggleSignin = () =>{
+      SetSignin(!issignin)
+  }
 
   return (
     <div className="flex justify-center my-10">
        <div className="card bg-slate-600 w-96 shadow-sm">
   <div className="card-body">
+
+    {!issignin && (
+      <div className='mx-3 '>
+      <h3 className="card-title">Username : {username}</h3>
+     
+       <input 
+      type="text" 
+      index = {username}
+      placeholder="Username" 
+      className="input input-md my-2" 
+      onChange={(e)=>SetUsername(e.target.value)}
+      />
+     
+      
+      </div>)}
     
     <div className='mx-3 '>
-     <h2 className="card-title">Email:{emailId} </h2>
+     <h3 className="card-title">Email:{emailId} </h3>
       <input 
       type="email" 
       index = {emailId}
@@ -24,7 +45,7 @@ const Login = () => {
       </div>
 
     <div className='mx-3 '>
-     <h2 className="card-title">Password:{password} </h2>
+     <h3 className="card-title">Password:{password} </h3>
       <input 
       index={password}
       type="text" 
@@ -36,8 +57,13 @@ const Login = () => {
     </div>
     
     <div className="card-actions justify-center">
-      <button className="btn btn-primary">Login</button>
+      <button className="btn btn-primary">
+        {issignin ? "SignIn" : "Sign Up"}
+        </button>
     </div>
+    <p className='m-2 cursor-pointer'
+        onClick={toggleSignin}
+        >New to Dev tinder? {issignin ? "Sign Up " :"Sign in"}</p>
   </div>
 </div>
     </div>
