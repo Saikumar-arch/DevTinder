fb3fbaeaf4949ecd571f307295e5978a4597a129
Saikumar-arch <iamsaikumar630@gmai.com>
2025-10-26 21:11:09 +0530

    building the funcanality that we con't access the profile and setting until we logged in

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
-import { Outlet } from 'react-router-dom'
+import { Await, Outlet, useNavigate } from 'react-router-dom'
 import Navbar from './Navbar.jsx'
 import Footer from './Footer.jsx'
+import axios from 'axios'
+import { BASE_URL } from '../Utility/constants.jsx'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../Utility/userSlice.jsx'
+import { useEffect } from 'react'
 
 const Body = () => {
+  
+  const dispatch = useDispatch()
+  const navigate  = useNavigate()
+  const userData  = useSelector((store) => store.user)
+
+  const fetchData = async() =>{
+    try{
+      const res = await axios.get(BASE_URL+"/api/profile",{
+        withCredentials : true
+      })
+      dispatch(addUser(res.data))
+    }
+    catch (err) {
+  if (err.response && err.response.status === 404) {
+    navigate("/login"); //redirect to login page
+  } else {
+    console.error(err);
+  }
+  }
+  }
+  useEffect(()=>{
+    if(!userData){fetchData()}
+    },[])
+
   return (
     <div>
         <Navbar/>
